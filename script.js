ee94ba90c61231f23d34af5b5014d3e27a47195c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2022-11-20 15:46:13 -0500

    Write function for creating grid of divs
    Uses two for loops to create sizeX row divs that each contain sizeY
    pixel divs.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
         if there is anything in the boxContainer delete it
 
         create the divs and append them to box container as children
+            for every sizeX create a div container that will be a "row" class
+            add to this sizeY children divs that are "pixel" class
 
         add hover events to each one
             if hover enter and left click down, then changeColor
@@ -21,5 +23,24 @@ boxContainer = document.querySelector(".boxContainer");
 
 const defaultSize = 16;
 
-let containerX = defaultSize;    
-let containerY = defaultSize;
+//Called once to set up default div grid
+createDivGrid(defaultSize,defaultSize);
+
+
+//Functions
+function createDivGrid(sizeX,sizeY){
+    for (x = 0; x < sizeX; x++){
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+
+        for (y = 0; y < sizeY; y++){
+            const pixelDiv = document.createElement("div");
+            pixelDiv.classList.add("pixel");
+            rowDiv.appendChild(pixelDiv);
+        }
+
+        boxContainer.appendChild(rowDiv);
+    }
+
+    return document.querySelector(".boxContainer");
+}
